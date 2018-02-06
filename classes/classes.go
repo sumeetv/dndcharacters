60e56247bc299789de4ff1e1c747761956a4c6c1
sumeetv <suvaidya@gmail.com>
2018-02-05 21:18:53 -0800

    Add stat ranking for all classes
    Summary: Add hard-coded stat preferences for all official classes
    
    Test Plan: build, run, generate chars
    
    Author: sumeetv
    
    Reviewers:

--- classes/classes.go
+++ classes/classes.go
@@ -38,6 +38,66 @@ func GetRankedStatsForClass(class string) []stats.AbilityScore {
 		stats.WisAbility,
 		stats.ChaAbility,
 	}
+	if class == "Barbarian" {
+		rankedScores = []stats.AbilityScore{
+			stats.ConAbility,
+			stats.StrAbility,
+			stats.DexAbility,
+			stats.ChaAbility,
+			stats.WisAbility,
+			stats.IntAbility,
+		}
+	}
+	if class == "Bard" {
+		rankedScores = []stats.AbilityScore{
+			stats.ChaAbility,
+			stats.DexAbility,
+			stats.ConAbility,
+			stats.WisAbility,
+			stats.StrAbility,
+			stats.IntAbility,
+		}
+	}
+	if class == "Cleric" {
+		rankedScores = []stats.AbilityScore{
+			stats.WisAbility,
+			stats.StrAbility,
+			stats.ConAbility,
+			stats.ChaAbility,
+			stats.DexAbility,
+			stats.IntAbility,
+		}
+	}
+	if class == "Druid" {
+		rankedScores = []stats.AbilityScore{
+			stats.WisAbility,
+			stats.DexAbility,
+			stats.ConAbility,
+			stats.ChaAbility,
+			stats.IntAbility,
+			stats.StrAbility,
+		}
+	}
+	if class == "Fighter" {
+		rankedScores = []stats.AbilityScore{
+			stats.StrAbility,
+			stats.ConAbility,
+			stats.DexAbility,
+			stats.ChaAbility,
+			stats.WisAbility,
+			stats.IntAbility,
+		}
+	}
+	if class == "Monk" {
+		rankedScores = []stats.AbilityScore{
+			stats.DexAbility,
+			stats.WisAbility,
+			stats.ConAbility,
+			stats.ChaAbility,
+			stats.IntAbility,
+			stats.StrAbility,						
+		}
+	}
 	if class == "Paladin" {
 		rankedScores = []stats.AbilityScore{
 			stats.StrAbility,
@@ -46,6 +106,56 @@ func GetRankedStatsForClass(class string) []stats.AbilityScore {
 			stats.DexAbility,
 			stats.WisAbility,
 			stats.IntAbility,
+		}
+	}
+	if class == "Ranger" {
+		rankedScores = []stats.AbilityScore{
+			stats.DexAbility,
+			stats.WisAbility,
+			stats.ConAbility,
+			stats.ChaAbility,
+			stats.IntAbility,
+			stats.StrAbility,
+		}
+	}
+	if class == "Rogue" {
+		rankedScores = []stats.AbilityScore{
+			stats.DexAbility,
+			stats.ChaAbility,
+			stats.StrAbility,
+			stats.ConAbility,
+			stats.WisAbility,
+			stats.IntAbility,
+		}
+	}
+	if class == "Sorcerer" {
+		rankedScores = []stats.AbilityScore{
+			stats.ChaAbility,
+			stats.DexAbility,
+			stats.ConAbility,
+			stats.WisAbility,
+			stats.IntAbility,
+			stats.StrAbility,
+		}
+	}
+	if class == "Warlock" {
+		rankedScores = []stats.AbilityScore{
+			stats.ChaAbility,
+			stats.ConAbility,
+			stats.DexAbility,
+			stats.WisAbility,
+			stats.StrAbility,
+			stats.IntAbility,
+		}
+	}
+	if class == "Wizard" {
+		rankedScores = []stats.AbilityScore{
+			stats.IntAbility,
+			stats.DexAbility,
+			stats.ConAbility,
+			stats.WisAbility,
+			stats.ChaAbility,
+			stats.StrAbility,
 		}
 	}
 
